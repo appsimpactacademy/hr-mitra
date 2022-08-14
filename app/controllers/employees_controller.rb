9b0da1fad914669a34e0df71bbcdb0642c7ac876
Ravi Singh <training.appsimpact@gmail.com>
2022-08-14 08:54:00 +0530

    completed CRUD for employees

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -6,4 +6,43 @@ class EmployeesController < ApplicationController
   def new
     @employee = Employee.new
   end
+
+  def create
+    @employee = Employee.new(employee_params)
+    if @employee.save
+      redirect_to employees_path, notice: 'Employee has been created successfully'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    if @employee.update(employee_params)
+      redirect_to employees_path, notice: 'Employee has been updated successfully'
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def destroy
+    @employee = Employee.find(params[:id])
+    if @employee.destroy
+      redirect_to employees_path, notice: 'Employee has been deleted successfully'
+    end
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :middle_name, :last_name, :personal_email, :city, :state, :country, :pincode, :address_line_1, :address_line_2)
+  end
 end
