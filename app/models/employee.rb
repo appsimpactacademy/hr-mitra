872e88a7c0001ee673d614f1f6eff40c1e365b6e
Ravi Singh <training.appsimpact@gmail.com>
2022-09-07 18:26:33 +0530

    added documents model for employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,7 @@
 class Employee < ApplicationRecord
+
+  has_many :documents
+
   validates :first_name, :last_name, presence: true
   validates :personal_email, presence: true, uniqueness: true
   validates :city, :state, :country, :pincode, :address_line_1, presence: true
